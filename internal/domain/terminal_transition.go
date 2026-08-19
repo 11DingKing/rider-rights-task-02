8249a0de1b3004b44b3a7a3406d92eb79f3fec16
ding <ding@ding.com>
2026-08-19 23:47:31 +0800

    bug: allow reopened closed rider rights cases

---
+++ internal/domain/terminal_transition.go
@@ -0,0 +1,8 @@
+package domain
+
+func terminalTransitionPolicy(from, to ItemStatus) bool {
+	if from != StatusCompleted && from != StatusCancelled {
+		return false
+	}
+	return to == StatusInProgress
+}
